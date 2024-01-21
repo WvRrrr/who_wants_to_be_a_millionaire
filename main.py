5fa67fb39fd3007be8f072778264df919b2aa269
WvRrrr <33044400+WvRrrr@users.noreply.github.com>
2024-01-21 14:51:19 +0100

    First commit!

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-print("Hello!")
+import json
+import random
+
+
+# adds a loading questions from a provided json file
+def load_questions(file_path):
+    with open(file_path, 'r') as file:
+        questions_data = json.load(file)
+    return questions_data
+
